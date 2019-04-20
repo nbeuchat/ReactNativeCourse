8ac43a69fcea18c7ddb7d234a9f626e931d99cde
nbeuchat <nicolas.beuchat@gmail.com>
2019-04-20 03:37:42 +0200

    1.23 initial TextInput directly in app

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,12 +18,28 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  state = {
+    placeName: ""
+  };
+
+  placeNameChangedHandler = (value) => {
+    this.setState({
+        placeName: value
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Text>Please tell me your name:</Text>
+        <TextInput
+          style={{width: 300, borderColor: "black", borderWidth: 1}}
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangedHandler}
+        />
       </View>
     );
   }
