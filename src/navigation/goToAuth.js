4222eac52953093918fb9f247578024c36671046
nbeuchat <nicolas.beuchat@gmail.com>
2019-04-24 22:03:15 +0200

    split navigation file by function

---
+++ src/navigation/goToAuth.js
@@ -0,0 +1,26 @@
+import { Navigation } from 'react-native-navigation';
+import { getScreenName } from '../../screens';
+
+export const goToAuth = () => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        id: 'Auth',
+        children: [
+          {
+            component: {
+              name: getScreenName('AuthScreen'),
+              options: {
+                topBar: {
+                  title: {
+                    text: 'Authentication'
+                  }
+                }
+              }
+            }
+          }
+        ]
+      }
+    }
+  });
+};
---
+++ src/navigation/goToMainTabs.js
@@ -0,0 +1,44 @@
+import { Navigation } from 'react-native-navigation';
+import { getScreenName } from '../../screens';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const goToMainTabs = () => {
+  Promise.all([
+    Icon.getImageSource('md-map', 30),
+    Icon.getImageSource('ios-share-alt', 30)
+  ]).then((sources) => {
+    Navigation.setRoot({
+      root: {
+        bottomTabs: {
+          id: 'MainTabsId',
+          children: [
+            {
+              component: {
+                name: getScreenName('FindPlaceScreen'),
+                options: {
+                  bottomTab: {
+                    fontSize: 12,
+                    text: 'Find Place',
+                    icon: sources[0]
+                  }
+                }
+              }
+            },
+            {
+              component: {
+                name: getScreenName('SharePlaceScreen'),
+                options: {
+                  bottomTab: {
+                    text: 'Share Place',
+                    fontSize: 12,
+                    icon: sources[1]
+                  }
+                }
+              }
+            }
+          ]
+        }
+      }
+    });
+  });
+};
